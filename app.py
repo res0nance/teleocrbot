000cde47116aa3b9cfd60c09babd9b4ea48a213e
res0nance <res0nance@users.noreply.github.com>
2017-09-15 23:25:44 +0800

    Initial app testing :snake:

---
+++ app.py
@@ -0,0 +1,27 @@
+import telepot
+import pprint
+import telepot.loop
+import os
+from PIL import Image, features
+import tesserocr
+
+telegram_botid = 'TELEGRAM_BOTID'
+
+bot = telepot.Bot(telegram_botid)
+
+def handle(msg):
+    pprint.pprint(msg)
+    if( 'sticker' in msg ):
+        path = os.path.dirname(__file__)
+        filename = 'temp.webp'
+        bot.download_file(msg['sticker']['file_id'], os.path.join(path,filename))
+        img = Image.open('temp.webp')
+        text = tesserocr.image_to_text(img)
+        bot.sendMessage(msg['chat']['id'], text)
+
+pprint.pprint(tesserocr.get_languages())
+pprint.pprint(bot.getMe())
+telepot.loop.MessageLoop(bot,handle).run_as_thread()
+
+while 1:
+    time.sleep(10)
